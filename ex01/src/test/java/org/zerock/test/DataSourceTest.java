bca7b2454bee43b8e2b68bb95deb7f3b4f90c0f3
engsk <engsk1211@gmail.com>
2020-03-01 02:17:05 +0900

    ex01 [2-1-complted] base configurations

---
+++ ex01/src/test/java/org/zerock/test/DataSourceTest.java
@@ -0,0 +1,42 @@
+package org.zerock.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class DataSourceTest {
+	
+	@Inject
+	private DataSource ds;
+	
+	@Test
+	public void testConnection() throws Exception{
+		try (Connection con = ds.getConnection()){
+			System.out.println(con);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		/*
+		try {
+			String url = "jdbc:oracle:thin:@localhost:1521/xepdb1";
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Connection con = DriverManager.getConnection(url, "zerock", "zerock12#");
+			System.out.println(con);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		*/
+	
+	}
+}
