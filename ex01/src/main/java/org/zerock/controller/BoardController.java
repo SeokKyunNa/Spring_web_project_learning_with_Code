7b7fca3dbc8788cf6facd1e3c5459a8483540ba4
engsk <engsk1211@gmail.com>
2020-03-04 02:22:46 +0900

    ex01 [2-3-3-3] solved korean encoding

--- ex01/src/main/java/org/zerock/controller/BoardController.java
+++ ex01/src/main/java/org/zerock/controller/BoardController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.BoardVO;
 import org.zerock.service.BoardService;
 
@@ -27,16 +28,14 @@ public class BoardController {
 	}
 	
 	@RequestMapping(value = "/register", method = RequestMethod.POST)
-	public String registerPOST(BoardVO board, Model model) throws Exception{
+	public String registPOST(BoardVO board, RedirectAttributes rttr) throws Exception{
 		
 		logger.info("regist post ...........");
 		logger.info(board.toString());
 		
 		service.regist(board);
 		
-		model.addAttribute("result", "success");
-		
-		//return "/board/success";
+		rttr.addFlashAttribute("msg", "SUCCESS");
 		return "redirect:/board/listAll";
 		
 	}
--- ex01/src/main/webapp/WEB-INF/views/board/register.jsp
+++ ex01/src/main/webapp/WEB-INF/views/board/register.jsp
@@ -1,6 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    
 <%@include file="../include/header.jsp"%>
+
 <form role="form" method="post">
 	<div class="box-body">
 		<div class="form_group">
@@ -21,4 +23,5 @@
 		<button type="submit" class="btn btn-primary">Submit</button>
 	</div>
 </form>
+
 <%@include file="../include/footer.jsp"%>
