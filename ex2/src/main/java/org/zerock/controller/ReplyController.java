f604ee003237f6e8d14f19a36d994a5d44c934be
engsk <engsk1211@gmail.com>
2020-03-10 23:59:49 +0900

    ex02 [3-3-2] reply list

--- ex2/src/main/java/org/zerock/controller/ReplyController.java
+++ ex2/src/main/java/org/zerock/controller/ReplyController.java
@@ -1,9 +1,12 @@
 package org.zerock.controller;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +21,7 @@ public class ReplyController {
 	@Inject
 	private ReplyService service;
 	
+	/* 댓글 등록 */
 	@RequestMapping(value = "", method = RequestMethod.POST)
 	public ResponseEntity<String> register(@RequestBody ReplyVO vo){
 		
@@ -28,6 +32,21 @@ public class ReplyController {
 		} catch (Exception e) {
 			e.printStackTrace();
 			entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		
+		return entity;
+	}
+	
+	/* 댓글 리스트 조회 */
+	@RequestMapping(value = "/all/{bno}", method = RequestMethod.GET)
+	public ResponseEntity<List<ReplyVO>> list(@PathVariable("bno") Integer bno){
+		
+		ResponseEntity<List<ReplyVO>> entity = null;
+		try {
+			entity = new ResponseEntity<List<ReplyVO>>(service.listReply(bno), HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
 		
 		return entity;
