6c32122ccd80dd98275dda9fb1ab4f2224bff3fb
engsk <engsk1211@gmail.com>
2020-03-23 23:59:37 +0900

    ex04 [5-8-1] file delete

--- ex04/src/main/java/org/zerock/service/BoardServiceImpl.java
+++ ex04/src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -44,15 +44,30 @@ public class BoardServiceImpl implements BoardService {
 		return dao.read(bno);
 	}
 
+	@Transactional
 	@Override
 	public void modify(BoardVO board) throws Exception {
 		
 		dao.update(board);
+		
+		Integer bno = board.getBno();
+		
+		String[] files = board.getFiles();
+		
+		if(files == null) {
+			return;
+		}
+		
+		for(String fileName : files) {
+			dao.replaceAttach(fileName, bno);
+		}
 	}
 
+	@Transactional
 	@Override
 	public void remove(Integer bno) throws Exception {
 		
+		dao.deleteAttach(bno);
 		dao.delete(bno);
 	}
 
