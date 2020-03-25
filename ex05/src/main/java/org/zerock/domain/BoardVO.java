c090460108c8b5ce7a7e8f5ebb09ff524e492521
engsk <engsk1211@gmail.com>
2020-03-25 22:27:45 +0900

    ex05 [6-2] copy ex04 project

---
+++ ex05/src/main/java/org/zerock/domain/BoardVO.java
@@ -0,0 +1,107 @@
+package org.zerock.domain;
+
+import java.util.Arrays;
+import java.util.Date;
+
+public class BoardVO {
+	
+	private Integer rnum;
+	private Integer bno;
+	private String title;
+	private String content;
+	private String writer;
+	private Date regdate;
+	private Date updatedate;
+	private int viewcnt;
+	private int replycnt;
+	
+	private String[] files;
+
+	public Integer getRnum() {
+		return rnum;
+	}
+
+	public void setRnum(Integer rnum) {
+		this.rnum = rnum;
+	}
+
+	public Integer getBno() {
+		return bno;
+	}
+
+	public void setBno(Integer bno) {
+		this.bno = bno;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+	public Date getUpdatedate() {
+		return updatedate;
+	}
+
+	public void setUpdatedate(Date updatedate) {
+		this.updatedate = updatedate;
+	}
+
+	public int getViewcnt() {
+		return viewcnt;
+	}
+
+	public void setViewcnt(int viewcnt) {
+		this.viewcnt = viewcnt;
+	}
+
+	public int getReplycnt() {
+		return replycnt;
+	}
+
+	public void setReplycnt(int replycnt) {
+		this.replycnt = replycnt;
+	}
+
+	public String[] getFiles() {
+		return files;
+	}
+
+	public void setFiles(String[] files) {
+		this.files = files;
+	}
+
+	@Override
+	public String toString() {
+		return "BoardVO [rnum=" + rnum + ", bno=" + bno + ", title=" + title + ", content=" + content + ", writer="
+				+ writer + ", regdate=" + regdate + ", updatedate=" + updatedate + ", viewcnt=" + viewcnt
+				+ ", replycnt=" + replycnt + ", files=" + Arrays.toString(files) + "]";
+	}
+	
+}
