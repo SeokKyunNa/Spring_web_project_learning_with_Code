910cbf06b7480a48c5225b9e5b876750e8bc173d
engsk <engsk1211@gmail.com>
2020-03-27 00:59:42 +0900

    ex_springboot [2-3] DB configuration in springboot

--- ex_springboot/src/test/java/org/zerock/ExSpringbootApplicationTests.java
+++ ex_springboot/src/test/java/org/zerock/ExSpringbootApplicationTests.java
@@ -1,13 +1,37 @@
 package org.zerock;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.sql.Connection;
 
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ExSpringbootApplicationTests {
 
+	@Autowired
+	private DataSource ds;
+	
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testConnection() throws Exception{
+		
+		System.out.println(ds);
+		
+		Connection con = ds.getConnection();
+		
+		System.out.println(con);
+		
+		con.close();
+		
 	}
 
 }
